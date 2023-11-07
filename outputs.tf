8c759ff4f0dd2be28060cfe1a960d2830859a91d
Nnamdi Victor Modebe <63635704+victordgr8t@users.noreply.github.com>
2023-11-07 13:18:08 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,31 @@
-output "ElasticFS_storage_dns_name" {
-  value       = aws_efs_file_system.ElasticFS_Storage.dns_name
+output "efs_storage_dns_name" {
+  value       = aws_efs_file_system.efs_storage.dns_name
   description = "EFS DNS name"
 }
 
-output "ElasticFS_storage_id" {
-  value       = aws_efs_file_system.ElasticFS_Storage.id
+output "efs_storage_id" {
+  value       = aws_efs_file_system.efs_storage.id
   description = "EFS ID"
 }
 
-output "ElasticFS_storage_arn" {
-  value       = aws_efs_file_system.ElasticFS_Storage.arn
+output "efs_storage_arn" {
+  value       = aws_efs_file_system.efs_storage.arn
   description = "EFS ARN"
 }
 
-output "ElasticFS_storage_access_point_id" {
-  value       = var.ap_directory != "" ? aws_efs_access_point.ElasticFS_Storage_access_point.id : null
+output "efs_storage_access_point_id" {
+  value       = var.ap_directory != "" ? aws_efs_access_point.efs_storage_access_point[0].id : null
   description = "EFS access point ID"
 }
 
-output "ElasticFS_storage_access_point_arn" {
-  value       = var.ap_directory != "" ? aws_efs_access_point.ElasticFS_Storage_access_point.arn : null
+output "efs_storage_access_point_arn" {
+  value       = var.ap_directory != "" ? aws_efs_access_point.efs_storage_access_point[0].arn : null
   description = "EFS access point ARN"
 }
 
-output "ElasticFS_storage_SG_id" {
-  value       = aws_security_group.efs_sg.id
+output "efs_security_group" {
+  value       = aws_security_group.nfs_sg.id
   description = "EFS Security Group ID"
 }
+
+
